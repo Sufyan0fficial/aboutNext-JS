b007f0c567cd95fde587e289115922b031f3523a
Muhammad Sufyan <computer388unofficial@gmail.com>
2024-08-11 18:11:30 +0500

    Home page changed

---
+++ 1st-nextjs-project/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+function HomePage() {
+  return (
+    <h1>
+      Hello, I am <strong>Muhammad Sufyan</strong> and this is my first Next.js
+      project.
+    </h1>
+  );
+}
+export default HomePage;
