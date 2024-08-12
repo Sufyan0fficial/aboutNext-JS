e8aa5737192fd0e21220b2951dfc3aa0bf81e856
Muhammad Sufyan <computer388unofficial@gmail.com>
2024-08-12 15:49:17 +0500

    2nd project added

---
+++ 2nd-nextjs-project/src/app/About/page.tsx
@@ -0,0 +1,15 @@
+import { table } from "console";
+import React from "react";
+
+function Name() {
+  return (
+    <div>
+      <h3>
+        My Name is{" "}
+        <strong className="text-red-700 text-1xl">MUHAMMAD SUFYAN</strong>
+      </h3>
+    </div>
+  );
+}
+
+export default Name;
---
+++ 2nd-nextjs-project/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h3>
+      This is the <strong className="text-red-700">HOME PAGE</strong>
+    </h3>
+  );
+}
