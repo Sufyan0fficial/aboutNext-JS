be0d34f07e10c800ed5ec2f95c19b896ae0061a3
Muhammad Sufyan <computer388unofficial@gmail.com>
2024-09-01 11:20:18 +0500

    highlight feature added

--- tic-tac-toe/src/app/page.tsx
+++ tic-tac-toe/src/app/page.tsx
@@ -2,10 +2,12 @@
 
 import { useState } from "react";
 
-function Square({ value, onSquareClick }: any) {
+function Square({ value, onSquareClick, isWinningSquare }: any) {
   return (
     <button
-      className="w-[100px] h-[100px] flex justify-center items-center border border-white font-bold text-7xl bg-slate-400 hover:bg-slate-500 "
+      className={`w-[100px] h-[100px] flex justify-center items-center border border-white font-bold text-7xl bg-slate-400 hover:bg-slate-500 ${
+        isWinningSquare ? "bg-yellow-400" : ""
+      }`}
       onClick={onSquareClick}
       style={{ color: value === "X" ? "red" : "blue" }}
     >
@@ -33,31 +35,34 @@ export default function MyApp() {
     setwhatNext(!whatNext);
   }
 
-  const winner = calculateWinner(squares);
+  const winnerInfo = calculateWinner(squares);
+  const winner = winnerInfo?.winner;
+  const winningLine = winnerInfo?.line;
+
   let status;
   let player;
   if (winner) {
-    status = `Winner : ${winner}`;
+    status = `Winner: ${winner}`;
   } else {
-    player = `Next Move : ${whatNext ? "X" : "O"}`;
+    player = `Next Move: ${whatNext ? "X" : "O"}`;
   }
+
   return (
     <div className="w-screen h-screen flex flex-col gap-4 justify-center items-center relative bg-gradient-to-r from-slate-950 to-slate-900">
       <div className="absolute top-0 text-4xl font-extrabold">Tic-Tac-Toe</div>
       <div className="font-semibold text-xl">{player}</div>
       <div className="flex gap-8">
         <div className="flex flex-wrap w-[320px] h-[320px] justify-evenly content-evenly">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          {squares.map((square, i) => (
+            <Square
+              key={i}
+              value={square}
+              onSquareClick={() => handleClick(i)}
+              isWinningSquare={winningLine?.includes(i)}
+            />
+          ))}
         </div>
-        <div className=" flex justify-center items-center text-6xl font-black ">
+        <div className=" flex justify-center items-center text-2xl lg:text-7xl font-black ">
           {status}
         </div>
       </div>
@@ -80,7 +85,7 @@ function calculateWinner(squares: any) {
   for (let i = 0; i < winningLines.length; i++) {
     const [a, b, c] = winningLines[i];
     if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], line: winningLines[i] };
     }
   }
 
